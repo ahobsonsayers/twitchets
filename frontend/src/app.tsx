1fd922ddd28aaf94a7d1e27c0bef47147aa9f186
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2025-08-15 21:17:22 +0100

    Use theme and config provider in App

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,23 +2,29 @@
 
 import { Button } from "./components/ui/button";
 import { GeneralSettings } from "./general-settings";
+import { ConfigProvider } from "./providers/config";
+import { ThemeProvider } from "./providers/theme";
+import type { Config } from "./types/config";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const config: Config = {
+    apiKey: "",
+    country: "GB",
+  };
   return (
-    <div className="container mx-auto space-y-6 p-6">
-      <div className="flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">
-          Application Configuration
-        </h1>
-      </div>
-
-      <GeneralSettings />
-
-      <div>{count}</div>
-    </div>
+    <ThemeProvider>
+      <ConfigProvider>
+        <div className="container mx-auto space-y-6 p-6">
+          <div className="flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
+            <h1 className="text-2xl font-bold sm:text-3xl">
+              Application Configuration
+            </h1>
+          </div>
+          <GeneralSettings config={config} />
+        </div>
+      </ConfigProvider>
+    </ThemeProvider>
   );
 }
 
