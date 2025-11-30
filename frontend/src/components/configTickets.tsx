228117b5a22cddf8f0811150e296a31614f118ff
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2025-11-30 20:57:17 +0000

    Make tickets card non collapsible

--- frontend/src/components/configTickets.tsx
+++ frontend/src/components/configTickets.tsx
@@ -3,6 +3,13 @@
 import { useConfig } from "../providers/config";
 import { CollapsibleCard } from "./cardCollapsible";
 import { Ticket } from "./configTicket";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import { Button } from "@/components/ui/button";
 import type { TicketConfig } from "@/types/config";
 import { Plus } from "lucide-react";
@@ -45,13 +52,16 @@ export function TicketsConfig() {
   };
 
   return (
-    <CollapsibleCard
-      title="Tickets Configuration"
-      description="Individual ticket configurations (overrides global settings)"
-      isOpen={isTicketsConfigOpen}
-      setIsOpen={setIsTicketsConfigOpen}
-      action={
+    <Card>
+      <CardHeader className="flex w-full items-center">
+        <div className="flex flex-col">
+          <CardTitle>Ticket Configuration</CardTitle>
+          <CardDescription>
+            Individual ticket configurations (overrides global settings)
+          </CardDescription>
+        </div>
         <Button
+          className="ml-auto"
           onClick={(e) => {
             e.stopPropagation();
             handleAddTicket();
@@ -60,30 +70,31 @@ export function TicketsConfig() {
           <Plus className="size-5" />
           Add Ticket
         </Button>
-      }
-    >
-      <div className="flex flex-col gap-y-4">
-        {config.tickets.length === 0 ? (
-          <p className="text-muted-foreground text-center">
-            No tickets configured. Click "Add Ticket" to get started.
-          </p>
-        ) : (
-          tickets.map((ticket, ticketIndex) => {
-            return (
-              <Ticket
-                key={ticketIndex}
-                ticketConfig={ticket}
-                globalConfig={config.global}
-                onUpdate={(updatedTicket) =>
-                  handleUpdateTicket(updatedTicket, ticketIndex)
-                }
-                onRemove={() => handleRemoveTicket(ticketIndex)}
-              />
-            );
-          })
-        )}
-      </div>
-    </CollapsibleCard>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-y-4">
+          {config.tickets.length === 0 ? (
+            <p className="text-muted-foreground text-center">
+              No tickets configured. Click "Add Ticket" to get started.
+            </p>
+          ) : (
+            tickets.map((ticket, ticketIndex) => {
+              return (
+                <Ticket
+                  key={ticketIndex}
+                  ticketConfig={ticket}
+                  globalConfig={config.global}
+                  onUpdate={(updatedTicket) =>
+                    handleUpdateTicket(updatedTicket, ticketIndex)
+                  }
+                  onRemove={() => handleRemoveTicket(ticketIndex)}
+                />
+              );
+            })
+          )}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
